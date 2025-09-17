bf3ee0744803a896a925bb29085d678e88577568
wootaepark <pakdal99@gmail.com>
2025-09-17 12:04:26 +0900

    initial commit

---
+++ src/main/java/com/liflay/liflayserver/LiflayServerApplication.java
@@ -0,0 +1,13 @@
+package com.liflay.liflayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiflayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiflayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liflay/liflayserver/LiflayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liflay.liflayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiflayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
